7eefa42d9ef767756333401be9ebc5c2fe85f96e
frogsword <113356699+frogsword@users.noreply.github.com>
2024-06-01 10:14:30 -0500

    fixed authentication

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,15 @@ import Login from "./pages/Login";
 import Home from "./pages/Home";
 import {ChakraProvider} from "@chakra-ui/react";
 import {AuthContext} from "./context/auth-context";
-import {useContext, useEffect, useState} from "react";
+import {useContext, useEffect} from "react";
 import Profile from "./pages/Profile";
 
 function App() {
-    const {authenticateUser} = useContext(AuthContext)
-
-    const [user, setUser] = useState({})
+    const {authenticateUser, authenticatedUser} = useContext(AuthContext)
 
     useEffect(() => {
         async function authenticate() {
-            const res = await authenticateUser()
-            setUser(res)
+            await authenticateUser()
         }
 
         authenticate()
@@ -24,8 +21,8 @@ function App() {
         <ChakraProvider>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<Home user={user}/>}></Route>
-                    <Route path="/profile/:name" element={<Profile currentUser={user}/>}></Route>
+                    <Route path="/" element={<Home user={authenticatedUser}/>}></Route>
+                    <Route path="/profile/:name" element={<Profile currentUser={authenticatedUser}/>}></Route>
                     <Route path="/login" element={<Login/>}></Route>
                 </Routes>
             </BrowserRouter>
--- src/context/auth-context.jsx
+++ src/context/auth-context.jsx
@@ -1,9 +1,11 @@
-import { createContext } from 'react';
+import {createContext, useState} from 'react';
 
 export const AuthContext = createContext(null)
 
 export const AuthContextProvider = (props) => {
-    const authenticateUser = async() => {
+    const [authenticatedUser, setUser] = useState(null);
+
+    const authenticateUser = async () => {
         const response = await fetch('http://localhost:8080/api/auth/authenticate', {
             method: "GET",
             mode: "cors",
@@ -13,9 +15,10 @@ export const AuthContextProvider = (props) => {
         if (res.status && res.status == 500) {
             return null
         }
+        setUser(res)
         return res
     }
 
-    const contextValue = {authenticateUser}
+    const contextValue = {authenticateUser, authenticatedUser}
     return <AuthContext.Provider value={contextValue}>{props.children}</AuthContext.Provider>
 }
