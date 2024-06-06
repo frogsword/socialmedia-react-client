336aa6a18a4ddfd7737c8d356cc393b40176cf9c
frogsword <113356699+frogsword@users.noreply.github.com>
2024-06-06 15:33:56 -0500

    added register page & styling

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,17 +3,19 @@ import {Button, FormControl, FormErrorMessage, Input, Spinner, Stack} from '@cha
 import {Link, useNavigate} from "react-router-dom";
 import "../styles/auth-form.css"
 
-function Login() {
+function Register() {
+    const [name, setName] = useState("")
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
     const [isError, setIsError] = useState(false)
     const [errMsg, setErrMsg] = useState("")
     const [loading, setLoading] = useState(false)
     const navigate = useNavigate()
 
     async function handleSubmit() {
-        if (email === '' || password === '') {
-            setErrMsg("Email and Password must not be empty.")
+        if (email === '' || password === '' || name == '') {
+            setErrMsg("Name, Email, and Password must not be empty.")
             setIsError(true)
         } else if (!email.includes("@")) {
             setErrMsg("Email must be valid.")
@@ -24,22 +26,25 @@ function Login() {
         } else if (password.length < 8) {
             setErrMsg("Password must be at least 8 characters long.")
             setIsError(true)
+        } else if (password !== confirmPassword) {
+            setErrMsg("Passwords must match")
+            setIsError(true)
         } else {
             setLoading(true)
 
-            await fetch('http://localhost:8080/api/auth/login', {
+            await fetch('http://localhost:8080/api/auth/signup', {
                 method: "POST",
                 mode: "cors",
                 credentials: "include",
                 headers: {
                     'content-type': 'application/json'
                 },
-                body: JSON.stringify({email, password})
+                body: JSON.stringify({name, email, password, confirmPassword})
             })
                 .then(() => {
                     setIsError(false)
                     setLoading(false)
-                    navigate("/")
+                    navigate("/login")
                 })
         }
     }
@@ -51,24 +56,29 @@ function Login() {
             )}
 
             <div className='auth-form'>
-                <h1>Login</h1>
+                <h1>Register</h1>
                 <FormControl isInvalid={isError}>
                     <Stack spacing={3}>
+                        <Input type="text" placeholder='Name' size='md' value={name} _placeholder={{color: "white"}}
+                               onChange={(e) => setName(e.target.value)}/>
                         <Input type="email" placeholder='Email' size='md' value={email} _placeholder={{color: "white"}}
                                onChange={(e) => setEmail(e.target.value)}/>
                         <Input type="password" placeholder='Password' size='md' value={password}
                                _placeholder={{color: "white"}}
                                onChange={(e) => setPassword(e.target.value)}/>
-                        <Button type="submit" colorScheme='teal' onClick={() => handleSubmit()}>Login</Button>
+                        <Input type="password" placeholder='Confirm Password' size='md' value={confirmPassword}
+                               _placeholder={{color: "white"}}
+                               onChange={(e) => setConfirmPassword(e.target.value)}/>
+                        <Button type="submit" colorScheme='teal' onClick={() => handleSubmit()}>Register</Button>
                     </Stack>
                     {isError && (
                         <FormErrorMessage>{errMsg}</FormErrorMessage>
                     )}
                 </FormControl>
-                <Link to={"/register"}>or register here</Link>
+                <Link to={"/login"}>or login here</Link>
             </div>
         </div>
     )
 }
 
-export default Login
+export default Register
